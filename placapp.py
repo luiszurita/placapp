e09dd853badf92066bdc6421492cb8a9a539b7fc
luis_zurita <zuritasilvaluiseduardo34@gmail.com>
2021-09-10 21:36:03 -0500

    Date and Time extraction, 2 ways

--- placapp.py
+++ placapp.py
@@ -3,14 +3,28 @@
 # the 'Pico & Placa' law along with more useful information.
 # by Luis Zurita
 
-import pywebio
-from pywebio.input import input, FLOAT, checkbox
+import pywebio #requires: tornado and user-agents
+import datetime
+from pywebio.input import input, checkbox, TEXT, DATE, TIME
 from pywebio.output import put_text
 
 def pipa():
-    license = input("Enter the license plate number: ", type=FLOAT)#string
-    c_date_time = checkbox(options=['use current date', 'use current time'])
+    license = input("Enter the license plate number: ",placeholder="AAA-000(0)"
+                                                      ,required=True)
+    c_date_time = checkbox(options=['use current date', 'use current time'],
+                           help_text='The fields not chosen will then be entered manually')
+    if 'use current date' not in c_date_time:
+        my_date = input("Enter date (dd/mm/yyyy): ", type=DATE, required=True)
+        my_date = datetime.datetime.strptime(my_date, "%Y-%m-%d").date()
+        weekday = my_date.strftime('%A')
+    # else:
 
+    if 'use current time' not in c_date_time:
+        my_time = input("Enter time (23:59): ", type=TIME, required=True)
+
+    print(my_date)
+    print(weekday)
+    print(my_time)
 
 if __name__ == '__main__':
     pywebio.start_server(pipa, port=80)
