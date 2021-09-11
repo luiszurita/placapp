84127839104e07eb2bfb7ed3f2c56a9a345b3dbf
luis_zurita <zuritasilvaluiseduardo34@gmail.com>
2021-09-10 19:08:07 -0500

    started developing the picoyplaca (pipa) function

--- placapp.py
+++ placapp.py
@@ -1,3 +1,16 @@
-# 'hello from main,now changed from dev'
-# 'this is a change in main'
-'this is a change in dev'
+# PlacApp: this is an App that receives a car's license plate and checks
+# if it should be currently transiting through Quito's streets according
+# the 'Pico & Placa' law along with more useful information.
+# by Luis Zurita
+
+import pywebio
+from pywebio.input import input, FLOAT, checkbox
+from pywebio.output import put_text
+
+def pipa():
+    license = input("Enter the license plate number: ", type=FLOAT)#string
+    c_date_time = checkbox(options=['use current date', 'use current time'])
+
+
+if __name__ == '__main__':
+    pywebio.start_server(pipa, port=80)
